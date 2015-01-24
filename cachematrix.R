25eb891736d4b8b885d5ce4cd211817fa6674e11
Spiralbaby <mono.veissid@gmail.com>
2015-01-24 12:59:49 -0500

    still testing

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function *** MONO IS TESTING THAT I CAN PUSH THIS, did not show up
+## Write a short comment describing this function *** CLEARED
 
 makeCacheMatrix <- function(x = matrix()) {
 
